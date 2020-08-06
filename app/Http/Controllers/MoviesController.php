f1d53100695ffa7902308dc548d95ef476352f08
SuhairShareef <suhairshareef1999@gmail.com>
2020-08-06 13:41:54 +0300

    Creaing API requests for Popular and now playing sections

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Carbon\Carbon;
+
 
 class MoviesController extends Controller
 {
@@ -13,7 +16,25 @@ class MoviesController extends Controller
      */
     public function index()
     {
-        //
+        // Sending a request for popular movies section 
+        $popularMovies = Http::withToken(config('services.moviesAPI.token'))->get(config('services.moviesAPI.baseURL').'movie/popular')->json()['results'];
+
+        // Sending a request for genres
+        $genresArray = Http::withToken(config('services.moviesAPI.token'))->get(config('services.moviesAPI.baseURL').'genre/movie/list')->json()['genres'];
+
+        // Mapping genres array
+        $genres = collect($genresArray)->mapWithKeys(function ($genre) {
+            return [$genre['id'] => $genre['name']];
+        });
+
+        // Sending a request for now playing movies section 
+        $nowPlayingMovies = Http::withToken(config('services.moviesAPI.token'))->get(config('services.moviesAPI.baseURL').'movie/now_playing')->json()['results'];
+
+        return view('index', [
+            'popularMovies' => $popularMovies,
+            'nowPlayingMovies' => $nowPlayingMovies,
+            'genres' => $genres,
+        ]);
     }
 
     /**
@@ -36,7 +57,7 @@ class MoviesController extends Controller
     {
         //
     }
-
+//
     /**
      * Display the specified resource.
      *
--- config/services.php
+++ config/services.php
@@ -29,5 +29,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'moviesAPI' => [
+        'token' => env('MOVIES_DB_TOKEN'),
+        'baseURL' => env('MOVIES_DB_URL'),
+    ],
 
 ];
