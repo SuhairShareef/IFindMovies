5a64ad73cb891c2533c0497b4342e349e06aada3
SuhairShareef <suhairshareef1999@gmail.com>
2020-08-09 16:08:44 +0300

    Creating the  search component and the results section

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class SearchDropdown extends Component
 {
+    public $search = '';
+
     public function render()
     {
         return view('livewire.search-dropdown');
